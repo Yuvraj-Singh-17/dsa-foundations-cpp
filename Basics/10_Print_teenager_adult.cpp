203c1caf0ccf93e3d734f4714b37176aec2db889
Yuvraj-Singh-17 <Yuvrajsinghsolankiq@gmail.com>
2026-06-16 17:31:29 +0000

    Added age for adult or teenager

---
+++ Basics/10_Print_teenager_adult.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a ;
+    cout << "Enter Age of the person : " ;
+    cin >> a ;
+    if ( a<18 )
+    cout << "The person is Teenager\n";
+    else 
+    cout << "The person is Adult\n" ;
+    return 0;
+}
