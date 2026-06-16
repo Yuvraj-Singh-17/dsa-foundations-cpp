8f0b028fe78b2a098855dff92ff49e43aa384ec7
Yuvraj-Singh-17 <Yuvrajsinghsolankiq@gmail.com>
2026-06-16 17:33:28 +0000

    You got discount or not

---
+++ Basics/13_Discount_got_or_not.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a ;
+    cout << "This program helps to know about that you're eligible for 65 Percent ticket discount or not \n" << "Enter your age : " ;
+    cin >> a ;
+    if (a>12)
+    cout << "You're not eligible for discounted price " ;
+    else 
+    cout << "Congratulation ! You're elgible for 65 Percent discount " ;
+    cout << "\n" ;
+}
